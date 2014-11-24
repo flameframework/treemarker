17cd31b54ae09a9ae17a461102adea70d86ddb9c
Michel Vollebregt <mvollebregt@xs4all.nl>
2014-11-24 22:29:43 +0100

    First version of FreemarkerWriterWrapper that writes output to a single writer (written to by free marker) to multiple files, using <file output="..."> tags.

--- src/main/scala/com/github/mvollebregt/flame/compiler/FreemarkerWriterWrapper.scala
+++ src/main/scala/com/github/mvollebregt/flame/compiler/FreemarkerWriterWrapper.scala
@@ -1,28 +1,59 @@
 package com.github.mvollebregt.flame.compiler
 
 import scala.collection.mutable.Stack
-import java.io.Writer
+import scala.util.matching.Regex
+import scala.util.matching.Regex._
+import java.io.{StringWriter, Writer}
 
 /**
  * Created by michel on 21-11-14.
  */
 class FreemarkerWriterWrapper(defaultFile: String, templateWriter: TemplateWriter) extends Writer {
 
-  private val writers = new Stack[Writer]
-  private val buffer = new StringBuilder
+  val startTagSingleQuote = "\\<&\\s*output\\s+file\\s*=\\s*'([^']*)'\\s*>"
+  val startTagDoubleQuote = "\\<&\\s*output\\s+file\\s*=\\s*\"([^\"]*)\"\\s*>"
+  val endTag = "\\</&\\s*output\\s*>"
 
-  private val out = templateWriter.getWriterFor(defaultFile)
+  val anyTag = new Regex(s"(?:$startTagSingleQuote)|(?:$startTagDoubleQuote)|($endTag)", "filesq", "filedq", "end")
+  private val buffer = new StringWriter()
+  val writers = new Stack[Writer]
+  writers.push(templateWriter.getWriterFor(defaultFile))
 
   override def write(cbuf: Array[Char], off: Int, len: Int): Unit = {
-    // detecteer <& en </& en push of pop dan een writer op de stack, anders gewoon schrijven
-    // als stack is leeg en er is een whitespace-karakter dan bufferen
-    // als stack is leeg en er is een non whitespace-karakter dan default writer op stack zetten
-    out.write(cbuf, off, len)
-
+    buffer.write(cbuf, off, len)
   }
 
-  override def flush(): Unit = for (writer <- writers) writer.flush()
+  override def flush(): Unit = ()
 
-  override def close(): Unit = for (writer <- writers) writer.close()
+  override def close(): Unit = writeAll(0)
+
+  private def writeAll(offset: Int) : Unit = {
+    val text = buffer.toString.substring(offset)
+    if (!text.isEmpty) {
+      val processed = anyTag.findFirstMatchIn(text) match {
+        case Some(m) => writePart(text.substring(0, m.start)); processTag(m)
+        case None => writePart(text)
+      }
+      writeAll(offset + processed)
+    }
+  }
+
+  private def writePart(text : String) : Int = {
+    writers.top.write(text)
+    text.length
+  }
+
+  private def processTag(m: Match) : Int = {
+    val file = Option(m.group("filesq")).orElse(Option(m.group("filedq")))
+    val endtag = Option(m.group("end"))
+    if (file.isDefined) {
+      writers.push(templateWriter.getWriterFor(file.get))
+    } else if (endtag.isDefined) {
+      writers.top.close
+      writers.pop
+    }
+    m.end
+  }
+
 
 }
--- src/test/scala/com/github/mvollebregt/flame/compiler/FreemarkerWriterWrapperTest.scala
+++ src/test/scala/com/github/mvollebregt/flame/compiler/FreemarkerWriterWrapperTest.scala
@@ -1,6 +1,6 @@
 package com.github.mvollebregt.flame.compiler
 
-import java.io.{ByteArrayOutputStream, BufferedOutputStream, StringWriter, PrintWriter}
+import java.io.{StringWriter, PrintWriter}
 
 import org.junit.runner.RunWith
 import org.mockito.Mockito._
@@ -14,20 +14,43 @@ import org.scalatest.FlatSpec
 @RunWith(classOf[JUnitRunner])
 class FreemarkerWriterWrapperTest extends FlatSpec with MockitoSugar {
 
-  val templateWriter = mock[TemplateWriter]
-  val defaultOutput = new StringWriter()
-  when(templateWriter.getWriterFor("default file")).thenReturn(defaultOutput)
-  val processor = new PrintWriter(new FreemarkerWriterWrapper("default file", templateWriter))
+  trait Fixture {
+    val templateWriter = mock[TemplateWriter]
+    val defaultOutput = new StringWriter()
+    when(templateWriter.getWriterFor("default file")).thenReturn(defaultOutput)
+    val processor = new PrintWriter(new FreemarkerWriterWrapper("default file", templateWriter))
+  }
 
-
-  "A template without file output markers" should "write a copy of the template to the default file" in {
-
+  "A template without file output markers" should "write a copy of the template to the default file" in new Fixture {
     // when
     processor.print("a template without file output markers")
-
+    processor.close
     // then
     assert(defaultOutput.toString == "a template without file output markers")
+  }
 
+
+  "A template with a file output marker with single quote" should "write the inner contents to the specified output" in new Fixture {
+    // given
+    val specifiedOutput = new StringWriter()
+    when(templateWriter.getWriterFor("specified name")).thenReturn(specifiedOutput)
+    // when
+    processor.println("<&output file='specified name'>inner contents</&output>")
+    processor.close
+    // then
+    assert(specifiedOutput.toString == "inner contents")
+  }
+
+
+  "A template with a file output marker with double quote" should "write the inner contents to the specified output" in new Fixture {
+    // given
+    val specifiedOutput = new StringWriter()
+    when(templateWriter.getWriterFor("another specified name")).thenReturn(specifiedOutput)
+    // when
+    processor.println("<&output file=\"another specified name\">other inner contents</&output>")
+    processor.close
+    // then
+    assert(specifiedOutput.toString == "other inner contents")
   }
 
 }
