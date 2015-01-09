62ddc8dc2b7421f165b3573a897299a7b2a1a968
Michel Vollebregt <michel@MacBook-Pro-van-Michel.fritz.box>
2015-01-09 23:51:15 +0100

    Added a parser that splits the contents of a tag into a tag name and a map of attributes

---
+++ src/main/scala/com/github/mvollebregt/flame/compiler/TagParser.scala
@@ -0,0 +1,28 @@
+package com.github.mvollebregt.flame.compiler
+
+/**
+ * Created by michel on 09-01-15.
+ */
+object TagParser {
+
+  private val splitEx = "(?:[^\\s=]+)(?:\\s*=\\s*(?:(?:\"[^\"]*\")|(?:'[^\"]*')|(?:[^\\s]*)))?".r
+
+  private def withoutQuotes(s: String) : String = {
+    if ((s.startsWith("\'") && s.endsWith("\'")) || (s.startsWith("\"") && s.endsWith("\""))) {
+      s.substring(1, s.length - 1)
+    } else s
+  }
+
+  def parse(content: String) : (String, Map[String, Option[String]]) = {
+
+    val elements : Seq[String] = (splitEx.findAllMatchIn(content).map(_.matched)).toSeq
+
+    (elements.head, elements.tail.map(attribute => {
+      attribute.indexOf("=") match {
+        case -1 => (attribute.trim, None)
+        case x => (attribute.substring(0, x).trim, (Some(withoutQuotes(attribute.substring(x + 1).trim))))
+      }}).toMap)
+
+  }
+
+}
---
+++ src/test/scala/com/github/mvollebregt/flame/compiler/TagParserTest.scala
@@ -0,0 +1,40 @@
+package com.github.mvollebregt.flame.compiler
+
+import org.scalatest.FlatSpec
+
+import com.github.mvollebregt.flame.compiler.TagParser._
+
+/**
+ * Created by michel on 09-01-15.
+ */
+class TagParserTest extends FlatSpec {
+
+  "The parser" should "parse a tag name" in {
+    // when
+    val value = parse("tagname")
+    // then
+    assert(value == ("tagname", Map()))
+  }
+
+  it should "parse a single attribute without quotes" in {
+    // when
+    val value = parse("other-tagname attribute=value")
+    // then
+    assert(value == ("other-tagname", Map("attribute" -> Some("value"))))
+  }
+
+  it should "trim spaces from attribute names and values" in {
+    // when
+    val value = parse("tagname attribute = value")
+    // then
+    assert(value == ("tagname", Map("attribute" -> Some("value"))))
+  }
+
+  it should "parse attributes with quotes" in {
+    // when
+    val value = parse("tagname first=\"1\" second='2'")
+    // then
+    assert(value == ("tagname", Map("first" -> Some("1"), "second" -> Some("2"))))
+  }
+
+}
